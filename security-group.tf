aac17a6c61666f72b96582f0e93d6d1576b9d117
Rufus Ajiboye <ruufman@gmail.com>
2023-12-02 13:17:53 +0000

    Files Added and Updated

---
+++ security-group.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "cicd_workflow" {
+  name        = "cicd_security_group"
+  description = "Allow SSH and HTTP Connection"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow inbound and outbound connection"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow inbound and outbound connection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH inbound and outbound connection"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "CI/CD COMPLETE"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "ubuntu_ami" {
+  description = "This is a variable to manage ec2 AMI type"
+  type        = string
+  default     = "ami-0694d931cee176e7d"
+}
+
+variable "VPC_value" {
+  description = "This is a variable to manage the VPC"
+  type        = string
+  default     = "vpc-0de121ec0ecf8eeb6"
+}
+
+variable "ec2_key_name" {
+  description = "This is a variable to manage ec2 key name"
+  type        = string
+  default     = "test100"
+}
+
+variable "ec2_instance_type" {
+  description = "This is a variable to manage ec2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "certificate_arn" {
+  description = "This variable will manage the HTPPS listener cert arn"
+  type        = string
+  default     = "arn:aws:acm:eu-west-1:622658514249:certificate/ca534fd3-96a8-494a-b182-6b71bad9696f"
+}
